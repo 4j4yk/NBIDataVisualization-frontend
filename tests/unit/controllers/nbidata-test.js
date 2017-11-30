5bfb071ac3d386dab80dac88b336bc6b266e6d66
akhampariya <akhampariya@unomaha.edu>
2017-11-30 18:51:43 -0500

    Update repo

---
+++ tests/unit/controllers/nbidata-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:nbidata', 'Unit | Controller | nbidata', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
